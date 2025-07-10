29b5c228cb3c5b63591e3608fefff036ceb7568b
Swastik Kumar Sahu <swastikkumarsahu1234@gmail.com>
2025-07-10 16:54:59 +0530

    Setup Spring Project

---
+++ mail-mind-spring/src/main/java/com/mail/mail_mind_spring/MailMindApplication.java
@@ -0,0 +1,13 @@
+package com.mail.mail_mind_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailMindApplication.class, args);
+	}
+
+}
---
+++ mail-mind-spring/src/test/java/com/mail/mail_mind_spring/MailMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mail.mail_mind_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
